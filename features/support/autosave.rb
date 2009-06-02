d449dd27a20f676a064aabae401adcf2ff40f334
maiha <maiha@wota.jp>
2009-06-02 09:11:14 +0900

    Created auto_save method to be called manually

--- features/support/autosave.rb
+++ features/support/autosave.rb
@@ -19,10 +19,14 @@ require 'pathname'
 module AutoSaveVisitedPage
   def visit(*args)
     val = super
+    auto_save
+    return val
+  end
+
+  def auto_save
     path = AutoSaveVisitedPage.auto_save_path
     path.open("w+") {|f| f.print response_body}
     auto_save_update_latest(path)
-    return val
   end
 
   def auto_save_update_latest(path)
@@ -53,7 +57,7 @@ module AutoSaveVisitedPage
   def self.init
     path = auto_save_path(:dir)
     if path.exist?
-      puts "[AutoSave] Cleaning #{path}"
+      puts "[AutoSave] Cleaning #{path} (#{Time.now})"
       FileUtils.rm_rf path
     end
   end
